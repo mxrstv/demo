09b62913b91d932f28ddfe5a416f128d79cf1dc4
sseezov <sseezov@mail.ru>
2024-11-20 00:20:00 +0700

    add scripts

--- server/app.js
+++ server/app.js
@@ -8,8 +8,8 @@ const fastify = Fastify({
 await client.connect();
 
 fastify.get('/partners', async (request, reply) => {
-  // const partners = await client.query('SELECT T1.id, T2.type, T1.name, T1.ceo, T1.phone, T1.rating FROM PARTNERS T1 JOIN organization_types T2 on T1.organization_type_id = T2.id;');
-  // const sales = await client.query('Select t2.id, sum(t1.production_quantity) from sales t1 join partners t2 on t1.partner_id = t2.id group by t2.id');
+  
+
 
   const partners = await client.query('SELECT T1.id, T2.type, T1.name, T1.ceo, T1.phone, T1.rating, SUM(T3.production_quantity) as total_quantity FROM PARTNERS T1 JOIN organization_types T2 on T1.organization_type_id = T2.id JOIN sales T3 on T3.partner_id = T1.id group by T1.id, T2.type, T1.name, T1.ceo, T1.phone, T1.rating;');
 
